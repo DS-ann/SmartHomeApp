06830a3ea6cd5dd8717a5ae6721233b98593c8a3
DS-ann <dsannigrahi5956@gmail.com>
2026-05-12 11:26:16 +0530

    Create MQTTController.kt

---
+++ app/src/main/java/com/example/ranjanasmarthome/MQTTController.kt
@@ -0,0 +1,38 @@
+package com.example.ranjanasmarthome
+
+import android.content.Context
+
+object MQTTController {
+
+    private var connected: Boolean = false
+    private var context: Context? = null
+
+    fun init(context: Context) {
+        this.context = context.applicationContext
+    }
+
+    fun setConnected(state: Boolean) {
+        connected = state
+    }
+
+    fun isConnected() = connected
+
+    fun sendCommand(cmd: String) {
+        if (!connected) return
+        println("MQTT command sent: $cmd")
+        // TODO: actual MQTT publish
+    }
+
+    fun onESPMessage(msg: String) {
+        context?.let { ctx ->
+            // Example parsing
+            if (msg.length >= 6) {
+                val light0 = msg[1] == '1'
+                val fan1 = msg[2] == '1'
+                val light4 = msg[5] == '1'
+                val fan5 = msg[6] == '1'
+                SmartHomeWidget.updateStateFromESP(light0, light4, fan1, fan5, ctx)
+            }
+        }
+    }
+}
