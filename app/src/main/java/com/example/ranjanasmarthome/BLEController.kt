3181c3b2182231c7c9468a3cccaa4b362fa22b7a
DS-ann <dsannigrahi5956@gmail.com>
2026-05-12 12:31:42 +0530

    Refactor BLEController to use BluetoothDevice in observers

--- app/src/main/java/com/example/ranjanasmarthome/BLEController.kt
+++ app/src/main/java/com/example/ranjanasmarthome/BLEController.kt
@@ -1,8 +1,7 @@
 package com.example.ranjanasmarthome
 
+import android.bluetooth.BluetoothDevice
 import android.util.Log
-import no.nordicsemi.android.ble.BleManager
-import no.nordicsemi.android.ble.data.Data
 import no.nordicsemi.android.ble.observer.ConnectionObserver
 
 private const val TAG = "BLEController"
@@ -12,36 +11,48 @@ object BLEController {
     private var bleManager: SmartHomeBleManager? = null
     var deviceConnected = false
 
+    /** Initialize BLEController with your BLE manager */
     fun init(manager: SmartHomeBleManager) {
         bleManager = manager
-        bleManager?.setConnectionObserver(object : ConnectionObserver {
-            override fun onDeviceConnected(deviceAddress: String) {
-                deviceConnected = true
-                Log.d(TAG, "BLE connected to $deviceAddress")
-            }
 
-            override fun onDeviceDisconnected(deviceAddress: String) {
-                deviceConnected = false
-                Log.d(TAG, "BLE disconnected from $deviceAddress")
-            }
-        })
+        // Observe connection events
+        bleManager?.observeConnection()
+            ?.subscribe(object : ConnectionObserver {
+                override fun onDeviceConnected(device: BluetoothDevice) {
+                    deviceConnected = true
+                    Log.d(TAG, "BLE connected to ${device.address}")
+                }
+
+                override fun onDeviceDisconnected(device: BluetoothDevice) {
+                    deviceConnected = false
+                    Log.d(TAG, "BLE disconnected from ${device.address}")
+                }
+            })
     }
 
+    /** Connect to a BLE device by address */
     fun connect(address: String) {
-        bleManager?.connect(address)?.enqueue()
+        val device = bleManager?.getBluetoothDevice(address)
+        device?.let {
+            bleManager?.connect(it)?.enqueue()
+        } ?: Log.e(TAG, "Cannot connect, device not found: $address")
     }
 
+    /** Disconnect from BLE device */
     fun disconnect() {
         bleManager?.disconnect()?.enqueue()
     }
 
+    /** Send command string over BLE */
     fun sendCommand(cmd: String) {
         if (deviceConnected) {
             bleManager?.sendCommand(cmd)
+        } else {
+            Log.w(TAG, "Cannot send, device not connected")
         }
     }
 
-    // Called whenever BLE receives data
+    /** Called whenever BLE receives data from the device */
     fun onMessageReceived(msg: String) {
         try {
             when {
